bf90d1c9847f08d5830d8667583286584e515a89
Xinran Guo <xguo@berkeley.edu>
2015-10-12 20:13:10 -0700

    Custom view controller and add a segue to navigate back

---
+++ TodoList/TodoList/ToDoListTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListTableViewController.swift
+//  TodoList
+//
+//  Created by Xinran Guo on 10/12/15.
+//  Copyright © 2015 Xinran Guo. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListTableViewController: UITableViewController {
+
+    @IBAction func cancelAddNewTask(segue: UIStoryboardSegue){
+        
+    }
+    
+    @IBAction func saveNewTask(segue: UIStoryboardSegue){
+        
+    }
+}
